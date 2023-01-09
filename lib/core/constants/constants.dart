98f4852926e8bb320da0c9168bfa29dc6b009606
Ömer Duran <102142305+OmerDuran46@users.noreply.github.com>
2023-01-09 14:53:35 +0300

    Update constants.dart

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -23,5 +23,5 @@ class Constants{
  }
   static String tKey=applicationTokenGet();
   static String customerKey=parolaGetir();
-  static String tDomain="http://omerduran.1ticaret.com/";
+  static String tDomain="";
 }
