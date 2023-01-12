c0e2bd1254580fbbd2efe5c80ac059e07fff9efb
Ömer Duran <102142305+OmerDuran46@users.noreply.github.com>
2023-01-12 15:30:49 +0300

    Update endpoint.dart

--- lib/core/constants/endpoint.dart
+++ lib/core/constants/endpoint.dart
@@ -1,26 +1,5 @@
 
 class Endpoints{
 
-  static String getFind="rest1/product/find";
-  static String getFavori="rest1/mobile/getFavoriteList";
-  static String deleteFavori="rest1/mobile/deleteFromFavoriteList";
-  static String getIndex="rest1/mobile/index";
-  static String getCategoriTree="rest1/category/getCategoryTree";
-  static String addToCart="rest1/cart/addToCart";
-  static String getCustomer="rest1/mobile/getCustomer";
-  static String getProductDetails="rest1/product/get";
-  static String getComments="rest1/product/getComments";
-  static String addFavorite="rest1/mobile/addToFavoriteList";
-  static String getCart="rest1/cart/getCart/";
-  static String addAlarm="rest1/mobile/addAlarm";
-  static String getAddress="rest1/mobile/getAddress";
-  static String deleteAddress="rest1/mobile/deleteAddress/";
-  static String updateCustomer="rest1/mobile/updateCustomer/";
-  static String customerLogin="rest1/customer/check";
-  static String applicationLogin="rest1/auth/login/";
-  static String deleteCartIndex="rest1/cart/removeItems";
-  static String getPriceAlarm="rest1/mobile/getAlarmList";
-  static String deleteAlarm="rest1/mobile/deleteAlarm/";
-  static String createCustomer="rest1/customer/addCustomers";
-  static String customerLoginWeb="rest1/customer/login";
+ 
 }
